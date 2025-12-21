fc9b0b767e576144edee3daf2b02f71ee366aa92
vivekayer <vrayer@gmail.com>
2025-12-21 15:48:25 +0530

    Added GA script

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-46LGGRPJ3F"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-46LGGRPJ3F');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
       </body>
